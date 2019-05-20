6671c5922bae4a9a390995d65d6d375f7006fc18
유진주 <yujinju@yujinjuui-MacBook-Pro.local>
2019-05-20 09:48:58 +0900

    ex

--- lib/login.dart
+++ lib/login.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'db.dart' as db;
+import 'db.dart' as db;git
 import 'profile_create.dart';
 
 class LoginPage extends StatefulWidget {
